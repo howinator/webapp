c3d354ccaedb9a639134c8f7d0584e3decc04569
Howie Benefiel <hben592@gmail.com>
2015-12-27 20:31:14 -0600

    Added doctor visit to admin so that we can add toy data to our models

--- meddjangoproject/the_big_one/admin.py
+++ meddjangoproject/the_big_one/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from .models import Doctor
 from .models import Specialty
+from .models import DoctorVisit
 
 admin.site.register(Doctor)
 admin.site.register(Specialty)
+admin.site.register(DoctorVisit)
 
 # Register your models here.
