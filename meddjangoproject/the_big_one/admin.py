e62e6b915b2c2b2ba15bf8cc4a3ba60b122cd0ff
Howie Benefiel <hben592@gmail.com>
2015-12-29 11:29:35 -0600

    Removed my entry from doctor visit

--- meddjangoproject/the_big_one/admin.py
+++ meddjangoproject/the_big_one/admin.py
@@ -3,12 +3,10 @@ from django.contrib import admin
 from .models import Doctor
 from .models import DoctorVisit
 from .models import Specialty
-from .models import DoctorVisit
 
 admin.site.registed(Address)
 admin.site.register(Doctor)
 admin.site.register(DoctorVisit)
 admin.site.register(Specialty)
-admin.site.register(DoctorVisit)
 
 # Register your models here.
