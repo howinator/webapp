9c89bab3e90a57c1c3d7a0f06a022267749ae936
Howie Benefiel <hben592@gmail.com>
2016-01-22 20:28:25 -0600

    Got jumbotron hompage set up and added links to ListView page.

--- meddjangoproject/the_big_one/models.py
+++ meddjangoproject/the_big_one/models.py
@@ -3,87 +3,91 @@ from localflavor.us.models import USZipCodeField
 from localflavor.us.models import USStateField
 from localflavor.us.models import PhoneNumberField
 
+
 class Address(models.Model):
-  street_address = models.CharField(max_length = 250)
-  city = models.CharField(max_length = 150)
-  state = USStateField(max_length = 2)
-  zip_code = USZipCodeField(max_length = 10)
-  is_active = models.BooleanField(
-      verbose_name = 'is address actively being used', 
-      default = True)
-  user_submitted = models.BooleanField(default = False)
+    street_address = models.CharField(max_length=250)
+    city = models.CharField(max_length=150)
+    state = USStateField(max_length=2)
+    zip_code = USZipCodeField(max_length=10)
+    is_active = models.BooleanField(
+        verbose_name='is address actively being used',
+        default=True)
+    user_submitted = models.BooleanField(default=False)
 
-  # Two time stamp fields
-  # auto_now_add time stamps when it is first created
-  date_entered = models.DateTimeField(
-      verbose_name = 'date+time address entered', 
-      auto_now_add = True)
-  # auto_now time stamps when the field is saved
-  date_last_updated = models.DateTimeField(
-      verbose_name = 'date+time last changed', 
-      auto_now = True)
+    # Two time stamp fields
+    # auto_now_add time stamps when it is first created
+    date_entered = models.DateTimeField(
+        verbose_name='date+time address entered',
+        auto_now_add=True)
+    # auto_now time stamps when the field is saved
+    date_last_updated = models.DateTimeField(
+        verbose_name='date+time last changed',
+        auto_now=True)
 
-  def __str__(self):
-    full_address = '''
+    def __str__(self):
+        full_address = '''
         {address}
         {city},
         {state} {zip_code}
     '''.format(
-          address=self.street_address,
-          city=self.city,
-          state=self.state,
-          zip_code=self.zip_code)
-    return full_address
+              address=self.street_address,
+              city=self.city,
+              state=self.state,
+              zip_code=self.zip_code)
+        return full_address
+
 
 class Specialty(models.Model):
-  # A model for storing the available specialties
-  # TODO Refine the many-to-many replationship to express the membership of a
-  # doctor in a specialty more effectively
+    # A model for storing the available specialties
+    # TODO Refine the many-to-many replationship to express the membership of a
+    # doctor in a specialty more effectively
 
-  specialty_name = models.CharField(max_length = 250)
+    specialty_name = models.CharField(max_length=250)
 
-  def __str__(self):
-    return self.specialty_name
+    def __str__(self):
+        return self.specialty_name
+
 
 class Doctor(models.Model):
-  first_name = models.CharField(max_length = 100)
-  last_name = models.CharField(max_length = 100)
-  # a doctor has one address, but multiple doctors can have same address
-  # therefore, many-to-one replationship
-  address = models.ForeignKey(Address, blank=True, null=True)
-  # doctor can have multiple specialties and specialties can belong to 
-  # multiple doctors therefore, many-to-many relationship
-  specialties = models.ManyToManyField(Specialty)
-  email_address = models.EmailField()
-  phone_number = PhoneNumberField(max_length = 9)
-  # This field should be automatically calculated by a script nightly
-  average_price = models.DecimalField(max_digits = 10, 
-      blank = True, null = True, decimal_places = 2)
-  date_entered = models.DateTimeField(
-      verbose_name = 'date+time doctor entered', 
-      auto_now_add = True)
-  date_last_updated = models.DateTimeField(
-      verbose_name = 'date+time last changed', 
-      auto_now = True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    # a doctor has one address, but multiple doctors can have same address
+    # therefore, many-to-one replationship
+    address = models.ForeignKey(Address, blank=True, null=True)
+    # doctor can have multiple specialties and specialties can belong to
+    # multiple doctors therefore, many-to-many relationship
+    specialties = models.ManyToManyField(Specialty)
+    email_address = models.EmailField()
+    phone_number = PhoneNumberField(max_length=9)
+    # This field should be automatically calculated by a script nightly
+    average_price = models.DecimalField(max_digits=10,
+                                        blank=True, null=True, decimal_places=2)
+    date_entered = models.DateTimeField(
+        verbose_name='date+time doctor entered',
+        auto_now_add=True)
+    date_last_updated = models.DateTimeField(
+        verbose_name='date+time last changed',
+        auto_now=True)
 
-  def __str__(self):
-    name = self.first_name + ' ' +  self.last_name
-    return name
+    def __str__(self):
+        name = self.first_name + ' ' + self.last_name
+        return name
+
 
 class DoctorVisit(models.Model):
-  doctor = models.ForeignKey(Doctor)
-  address = models.ForeignKey(Address)
-  visit_date = models.DateTimeField('Date of Visit')
-  procedure = models.CharField('Procedure', max_length=250)
-  price = models.DecimalField('Price', decimal_places=2, max_digits=9)
+    doctor = models.ForeignKey(Doctor)
+    address = models.ForeignKey(Address)
+    visit_date = models.DateTimeField('Date of Visit')
+    procedure = models.CharField('Procedure', max_length=250)
+    price = models.DecimalField('Price', decimal_places=2, max_digits=9)
 
-  def showPrice():
-    return '$' + str(self.price)
+    def showPrice():
+        return '$' + str(self.price)
 
-  def __str__(self):
-    doctor_string = str(self.doctor)
-    address_string = str(self.address)
-    full_output = '''
+    def __str__(self):
+        doctor_string = str(self.doctor)
+        address_string = str(self.address)
+        full_output = '''
       Doctor : {doctor}
 
       Address : {address}
@@ -94,9 +98,9 @@ class DoctorVisit(models.Model):
 
       price : {price}
     '''.format(
-        doctor_string=str(self.doctor),
-        address_string=str(self.address),
-        visit_date=str(self.visit_date),
-        procedure=self.procedure,
-        price=self.showPrice())
-    return full_output 
+            doctor_string=str(self.doctor),
+            address_string=str(self.address),
+            visit_date=str(self.visit_date),
+            procedure=self.procedure,
+            price=self.showPrice())
+        return full_output
--- meddjangoproject/the_big_one/views.py
+++ meddjangoproject/the_big_one/views.py
@@ -7,75 +7,75 @@ from django.core.urlresolvers import reverse
 from .models import Doctor
 from .forms import AddressForm, DoctorForm
 
+
 def new_doctor(request):
-  
-  if request.method == 'POST':
-    print(request.POST)
-    print("REQUEST GET!!", request.GET)
-    print("request.method == POST")
-    doctor_form = DoctorForm(request.POST, prefix = "doctor")
-    print("instantiated DoctorForm", doctor_form.is_bound)
-    address_form = AddressForm(request.POST, prefix = "address")
-    print("instantiated addressform")
-    print(doctor_form['first_name'])
-    print("doc_form valid", doctor_form.is_valid())
+    if request.method == 'POST':
+        print(request.POST)
+        print("REQUEST GET!!", request.GET)
+        print("request.method == POST")
+        doctor_form = DoctorForm(request.POST, prefix="doctor")
+        print("instantiated DoctorForm", doctor_form.is_bound)
+        address_form = AddressForm(request.POST, prefix="address")
+        print("instantiated addressform")
+        print(doctor_form['first_name'])
+        print("doc_form valid", doctor_form.is_valid())
 
-    if doctor_form.is_valid() and address_form.is_valid():
-      # Save the address object to database first so that it can be used
-      # by the doctor object
-      print("forms valid")
-      address = address_form.save()
-      doctor = doctor_form.save(commit = False)
-      doctor.address = address
-      doctor.save()
-      return HttpResponseRedirect(reverse('doctor_list'))
-  else:
-    doctor_form = DoctorForm(prefix = "doctor")
-    print("doc_form get instant")
-    address_form = AddressForm(prefix = "address")
-    print("address_form get instant")
+        if doctor_form.is_valid() and address_form.is_valid():
+            # Save the address object to database first so that it can be used
+            # by the doctor object
+            print("forms valid")
+            address = address_form.save()
+            doctor = doctor_form.save(commit=False)
+            doctor.address = address
+            doctor.save()
+            return HttpResponseRedirect(reverse('doctor_list'))
+    else:
+        doctor_form = DoctorForm(prefix="doctor")
+        print("doc_form get instant")
+        address_form = AddressForm(prefix="address")
+        print("address_form get instant")
 
-  return render(request, 
-                'doctors/create_doctor.html',
-                {'doctor_form': doctor_form, 'address_form': address_form})
-
+    return render(request,
+                  'doctors/create_doctor.html',
+                  {'doctor_form': doctor_form, 'address_form': address_form})
 
 
 class DoctorListView(ListView):
 
-  model = Doctor
-  template_name = "doctors/doctor_list.html"
-  context_object_name = "submitted_doctors"
+    model = Doctor
+    template_name = "doctors/doctor_list.html"
+    context_object_name = "submitted_doctors"
+
 
 class DoctorDetailView(DetailView):
 
-  model = Doctor
-  template_name = "doctors/doctor_detail.html"
+    model = Doctor
+    template_name = "doctors/doctor_detail.html"
 
 # TODO This needs to get turned into an actual 404 page that re-directs to
 # either doctors or doctor visits
+
+
 def core_404_page(request):
-  return HttpResponse("Oops! This page doesn't exist. Please navigate to \
+    return HttpResponse("Oops! This page doesn't exist. Please navigate to \
   another page.")
 
 
 
-
-
+# -----------------------------------------------------
 # Deprecated FBVs below this line
 
 # FBV for showing Doctor Detail
-#def doctor(request, doctor_id):
+# def doctor(request, doctor_id):
 #  doctor = get_object_or_404(Doctor, pk=doctor_id)
 #
 #  return render(request, 'doctors/doctor_detail.html', {'doctor': doctor})
 
 
 # FBV for doctor listing in case I ever need it
-# Takes variable number of argumnets because the trailing slash from the url 
+# Takes variable number of argumnets because the trailing slash from the url
 # gets fed to the view function
 # def doc_index(request, *arg):
 #  latest_doctor_list = Doctor.objects.order_by('-date_entered')[:5]
 #  context = {'latest_doctor_list': latest_doctor_list}
 #  return render(request, 'doctors/index.html', context)
-
